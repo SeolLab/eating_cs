a02e29bcfc863869f1bea652ce83e7e4e964f667
Oxalidaceae <83827831+Oxalidaceae@users.noreply.github.com>
2021-05-15 11:58:13 +0900

    서준수 9주차 1

--- 1.c
+++ 1.c
@@ -1,18 +1,18 @@
+//바둑판 흰 돌, 검은 돌
 #include <stdio.h>
-
-
-int main(void) {
-	int array[10] = { 0, 3, 5, 4, 1, 8, 9, 12, 6, 3 };
-
-	int i=0;
-	int k;
-
-	for (k = 0; k < 10; k++) {
-		if (array[i] <= array[k]) {
-			i = k;
-		}
+void main() {
+	int baduk[12] = { 1,0,2,0,1,1,2,0,2,1,0,1 };
+	//0은 없음, 1은 검은 돌, 2는 흰 돌
+	int i, x, y;
+	for (i = 0; i < 12; i++) {
+		x = i % 4 + 1;
+		y = i / 4 + 1;
+		printf("%d행 %d열에", y, x);
+		if (baduk[i] == 1)
+			printf(" 검은 돌이 놓여 있습니다.\n");
+		else if (baduk[i] == 2)
+			printf(" 흰 돌이 놓여 있습니다.\n");
+		else
+			printf("는 돌이 놓여 있지 않습니다.\n");
 	}
-	printf("The largest value in an array-of-int is : %d\n\n", array[i]);
-
-	return 0;
 }

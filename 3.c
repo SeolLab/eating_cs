0cd8e9eed02a07b236d523327acb31d3c85d0abd
Oxalidaceae <83827831+Oxalidaceae@users.noreply.github.com>
2021-05-15 11:59:09 +0900

    서준수 9주차 3

--- 3.c
+++ 3.c
@@ -1,36 +1,19 @@
+//³ª¸ÓÁö(3052)
 #include <stdio.h>
-#define MONTHS 12
-#define YEARS 5
-
-int main(void) {
-	const float rain[YEARS][MONTHS] = {
-		{4.3,4.3,4.3,3.0,2.0,1.2,0.2,0.2,0.4,2.4,3.5,6.6},
-		{8.5,8.2,1.2,1.6,2.4,0.0,5.2,0.9,0.3,0.9,1.4,7.3},
-		{9.1,8.5,6.7,4.3,2.1,0.8,0.2,0.2,1.1,2.3,6.1,8.4},
-		{7.2,9.9,8.4,3.3,1.2,0.8,0.4,0.0,0.6,1.7,4.3,6.2},
-		{7.6,5.6,3.8,2.8,3.8,0.2,0.0,0.0,0.0,1.3,2.6,5.2}
-	};
-	int year, month;
-	float subtot, total;
-
-	printf(" YEAR      RAINFALL   (inches)\n");
-	for (year = 0, total = 0; year < YEARS; year++) {
-		for (month = 0, subtot = 0; month < MONTHS; month++)
-			subtot += rain[year][month];
-		printf("%5d %15.1f\n", 2010 + year, subtot);
-		total += subtot;
+int main() {
+	int arr[10];
+	int res = 0;
+	for (int i = 0; i < 10; i++)
+		scanf("%d", &arr[i]);
+	for (int k = 0; k < 10; k++) {
+		int count = 0;
+		arr[k] = arr[k] % 42;
+		for (int j = 0; j < k; j++) {
+			if (arr[k] == arr[j])
+				count++;
+		}
+		if (count == 0)
+			res++;
 	}
-	printf("\nThe yearly average is %.1f inches. \n\n", total / YEARS);
-	printf("MONTHLY AVERAGES:\n\n");
-	printf(" Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec\n");
-
-	for (month = 0; month < MONTHS; month++)
-	{
-		for (year = 0, subtot = 0; year < YEARS; year++)
-			subtot += rain[year][month];
-		printf("%4.1f", subtot / YEARS);
-	}
-	printf("\n");
-
-	return 0;
+	printf("%d", res);
 }
